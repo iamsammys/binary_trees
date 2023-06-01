8faaee9af91012856687919d4629969413000d99
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-01 06:55:01 +0100

    added a new function to check if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,12 +8,8 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node)
-		return (0);
-
-	if (node->parent && (!node->right && !node->left))
+	if (node && (!node->right && !node->left))
 			return (1);
-	else
-		return (0);
+	return (0);
 }
 	
