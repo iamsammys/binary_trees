9050c15e29276db7de82ca2ea3a0a0f5d680e508
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-01 16:15:00 +0100

    added a function that calculates number of nodes with atleast one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_nodes - counts the nodes with
+ * binary_tree_nodes - counts the nodes with
  * at least 1 child in a binary tree
  * @tree: pointer to the root node of the
  * tree to count the number of nodes
